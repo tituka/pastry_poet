8a6f28ee7aa8ac8fc4c599d2be7afcedb5cd76f3
tiina <tiina@4-mobile.org>
2019-06-18 19:49:48 +0200

    Adding some files

---
+++ tue.py
@@ -0,0 +1,152 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import tensorflow as tf
+from tensorflow.keras.callbacks import EarlyStopping
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+base_dir = "/home/tiina/vegetables/webcam"
+
+
+#_URL = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+
+#zip_file = tf.keras.utils.get_file(origin=_URL, fname="flower_photos.tgz", extract=True)
+
+#base_dir = os.path.join(os.path.dirname(zip_file), 'flower_photos')
+
+IMAGE_SIZE = 224
+BATCH_SIZE = 32
+
+datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+    rescale=1./255, 
+    validation_split=0.2)
+
+train_generator = datagen.flow_from_directory(
+    base_dir,
+    target_size=(IMAGE_SIZE, IMAGE_SIZE),
+    batch_size=BATCH_SIZE, 
+    subset='training')
+
+val_generator = datagen.flow_from_directory(
+    base_dir,
+    target_size=(IMAGE_SIZE, IMAGE_SIZE),
+    batch_size=BATCH_SIZE, 
+    subset='validation')
+
+for image_batch, label_batch in train_generator:
+  image_batch.shape, label_batch.shape
+  break
+image_batch.shape, label_batch.shape
+
+
+print (train_generator.class_indices)
+
+labels = '\n'.join(sorted(train_generator.class_indices.keys()))
+
+with open('labels_own.txt', 'w') as f:
+  f.write(labels)
+
+IMG_SHAPE = (IMAGE_SIZE, IMAGE_SIZE, 3)
+
+# Create the base model from the pre-trained model MobileNet V2
+base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
+                                              include_top=False, 
+                                              weights='imagenet')
+
+base_model.trainable = False
+model = tf.keras.Sequential([
+  base_model,
+  tf.keras.layers.Conv2D(32 ,3,  activation='relu'),
+  tf.keras.layers.Dropout(0.5),
+  tf.keras.layers.GlobalAveragePooling2D(),
+  tf.keras.layers.Dense(25, activation='softmax')
+])
+model.compile(optimizer=tf.keras.optimizers.Adam(), 
+              loss='categorical_crossentropy', 
+              metrics=['accuracy'])
+
+model.summary()
+
+print('Number of trainable variables = {}'.format(len(model.trainable_variables)))
+
+epochs = 12
+
+es= EarlyStopping(
+        #stop training when `val_loss` is no longer improving
+        monitor='val_loss',
+        # "no longer improving" being defined as "no better than 1e-2 less"
+        min_delta=1e-2,
+        # "no longer improving" being further defined as "for at least 2 epochs"
+        patience=4,
+        verbose=1)
+
+log_dir="logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+history = model.fit(train_generator, 
+                    epochs=epochs, callbacks=[es, tensorboard_callback],
+                    validation_data=val_generator)
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+plt.figure(figsize=(8, 8))
+plt.subplot(2, 1, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.ylabel('Accuracy')
+plt.ylim([min(plt.ylim()),1])
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(2, 1, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.ylabel('Cross Entropy')
+plt.ylim([0,1.0])
+plt.title('Training and Validation Loss')
+plt.xlabel('epoch')
+plt.show()
+plt.savefig('21_own.png')
+saved_model_dir = 'save/fine_tuning_own1'
+
+tf.saved_model.save(model, saved_model_dir)
+
+
+'''
+base_model.trainable = True
+
+# Let's take a look to see how many layers are in the base model
+print("Number of layers in the base model: ", len(base_model.layers))
+
+# Fine tune from this layer onwards
+fine_tune_at = 100
+
+# Freeze all the layers before the `fine_tune_at` layer
+for layer in base_model.layers[:fine_tune_at]:
+  layer.trainable =  False
+
+model.compile(loss='categorical_crossentropy',
+              optimizer = tf.keras.optimizers.Adam(1e-5),
+              metrics=['accuracy'])
+
+model.summary()
+
+print('Number of trainable variables = {}'.format(len(model.trainable_variables)))
+
+history_fine = model.fit(train_generator, 
+                         epochs=2,
+                         validation_data=val_generator)
+
+saved_model_dir = 'save/fine_tuning_own'
+tf.saved_model.save(model, saved_model_dir)
+'''
+converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir)
+tflite_model = converter.convert()
+
+with open('model_own2.tflite', 'wb') as f:
+  f.write(tflite_model)
